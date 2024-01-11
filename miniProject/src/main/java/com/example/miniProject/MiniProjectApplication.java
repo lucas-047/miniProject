04554f353771d7d1658cb8c9371a3b832c46c7cf
Four <vrajsutariya94086@gmail.com>
2024-01-12 00:07:39 +0530

    commit

--- miniProject/src/main/java/com/example/miniProject/MiniProjectApplication.java
+++ miniProject/src/main/java/com/example/miniProject/MiniProjectApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = "com.example.miniProject")
 public class MiniProjectApplication {
 
 	public static void main(String[] args) {
