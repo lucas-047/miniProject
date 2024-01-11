b21f089ccb87adae3b305bb24d53b5cd3ac577ff
Four <vrajsutariya94086@gmail.com>
2024-01-11 15:12:23 +0530

    Initial commit

---
+++ miniProject/src/main/java/com/example/miniProject/MiniProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.miniProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjectApplication.class, args);
+		System.out.println("vasu lodo");
+	}
+
+}
