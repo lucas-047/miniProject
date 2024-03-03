7aa60c0c879ea0ae27a1946b782e78ef8f5b5a7e
Four <vrajsutariya94086@gmail.com>
2024-03-03 22:26:07 +0530

    Issue/Return Book

--- miniProject/src/main/java/com/library/Config/SecurityConfig.java
+++ miniProject/src/main/java/com/library/Config/SecurityConfig.java
@@ -62,7 +62,8 @@ public class SecurityConfig {
             "return",
             "returning",
             "/user/**",
-            "/admin/**"
+            "/admin/**",
+            "/**"
 
     };
     @Bean
