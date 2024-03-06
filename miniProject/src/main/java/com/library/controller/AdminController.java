21d3dbd631cb8a56f91c90cca8563017fa0652b8
vasu <kalavadiya.vasu@gmail.com>
2024-03-06 16:17:50 +0530

    try

--- miniProject/src/main/java/com/library/controller/AdminController.java
+++ miniProject/src/main/java/com/library/controller/AdminController.java
@@ -3,12 +3,15 @@ package com.library.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.sql.SQLOutput;
+
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
     @RequestMapping("/index")
     public String dashBorad(){
+        System.out.println("ghfhe");
         return "Admin/Admin_dashBoard";
     }
 
